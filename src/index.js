1ab1ae8666884b04aeaebe53d0c2734a1ffdb430
Mark Jason Aquiatin <marcus@Marks-MacBook-Pro.local>
2018-09-02 05:52:31 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader';
+import './scss/main.scss';
+console.log(process.env.NODE_ENV);
+
+class App extends React.Component {
+  render() {
+    return (
+      <div><h1>Hello, Worlds!</h1></div>
+    );
+  }
+}
+
+const Root = hot(module)(App);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+console.log(process.env.ENV);
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js'
+  },
+  module: {
+   rules: [
+     {
+       test: /\.(js|jsx)$/,
+       exclude: /node_modules/,
+       use: {
+         loader: 'babel-loader'
+       }
+     },
+     {
+       test: /\.(scss|sass)$/,
+       use: [
+         'style-loader',
+        //  MiniCssExtractPlugin.loader,
+         'css-loader',
+         'postcss-loader',
+         'sass-loader'
+       ]
+     }
+   ]
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {}),
+    // new MiniCssExtractPlugin({
+    //   filename: 'style.[contenthash].css',
+    // }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash(),
+    new webpack.HotModuleReplacementPlugin(),
+    new Dotenv()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    watchContentBase: true,
+    compress: true,
+    disableHostCheck: true,
+    historyApiFallback: true,
+    hot: true,
+    host: '0.0.0.0',
+    https: true,
+    port: 3000,
+  }
+}
